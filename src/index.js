50659244a00bf4ab668ea08f8982847215d0e4fa
vkolyago <v.kolyago@artox.com>
2022-02-22 22:59:37 +0300

    feat: add function reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+let abs = Math.abs(n);
+let str =  abs.toString();
+let arr = str.split("");
+var prepareResult = arr.reverse();
+let result = prepareResult.join(""); 
+return result;
 }
